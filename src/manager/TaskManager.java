415c741d287f82e3ba46605075b3307a65e2142f
Magomed Keligov <kms_91@list.ru>
2025-04-14 21:33:05 +0300

    Подготовленное ФЗ 6 v1.1

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -9,27 +9,34 @@ import java.util.ArrayList;
 public interface TaskManager {
     //выношу список методов для объекта Task;
     Task saveTask(Task task);
+
     ArrayList<Task> getAllTasks();
+
     Task getTaskById(int id);
+
     void updateTask(Task task);
     void deleteTaskById(int id);
 
-    //выношу список методов для объекта Subtask;
-    Subtask saveSubtask(Subtask subtask);
+    Subtask saveSubtask(Subtask subtask);//выношу список методов для объекта Subtask;
+
     ArrayList<Subtask> getAllSubtasks();
+
     Subtask getSubtaskById(int id);
+
     void updateSubtask(Subtask subtask);
     void deleteSubtaskById(int id);
 
     //выношу список методов для объекта Epic;
     Epic saveEpic(Epic epic);
+
     ArrayList<Epic> getAllEpics();
+
     Epic getEpicById(int id);
+
     void updateEpic(Epic epic);
     void deleteEpicById(int id);
+    
+    ArrayList<Subtask> getSubtasksByEpicId(int epicId);//Создаю метод для получения всех подзадач определённого эпика;
 
-    //Создаю метод для получения всех подзадач определённого эпика;
-    ArrayList<Subtask> getSubtasksByEpicId(int epicId);
-    //создаю метод для истории просмотров;
-    ArrayList<Task> getHistory();
+    ArrayList<Task> getHistory();//создаю метод для истории просмотров;
 }
