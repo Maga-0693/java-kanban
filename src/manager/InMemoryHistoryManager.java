0f46569d97ffd448a5aad68e544802389379e0a7
Magomed Keligov <kms_91@list.ru>
2025-04-15 12:35:06 +0300

    Подготовленное ФЗ 6 v1.7

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -6,44 +6,51 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
     public class InMemoryHistoryManager implements HistoryManager {
-        private Node head;
-        private Node tail;
+        private Node head; //первый элемент двусвязного списка
+        private Node tail; //последний элемент двусвязного списка
         private final HashMap<Integer, Node> nodeMap = new HashMap<>();
 
         @Override
         public void add(Task task) {
+            // Если задача есть в истории, то сначала удаляем старую версию
             if (nodeMap.containsKey(task.getId())) {
                 removeNode(nodeMap.get(task.getId()));
             }
+            //добавляемв конец списка
             linkLast(task);
         }
 
         @Override
         public ArrayList<Task> getHistory() {
 
-            return getTasks();
+            return getTasks(); //Возвращаю список задач из истории
         }
 
         @Override
         public void remove(int id) {
-            Node node = nodeMap.get(id);
+            Node node = nodeMap.get(id); //Нахожу узел по id
             if (node != null) {
-                removeNode(node);
+                removeNode(node); //Удаляю узел из списка
             }
         }
 
+        //Добавление задачи в конец двусвязного списка
         private void linkLast(Task task) {
             Node newNode = new Node(task);
+            //Если список пустой, то новый узел становится и в начало и в конец
             if (tail == null) {
                 head = tail = newNode;
             } else {
+                //Иначе добавляем в конец списка
                 tail.next = newNode;
                 newNode.prev = tail;
                 tail = newNode;
             }
+            //сохраняем узел для быстрого доступа
             nodeMap.put(task.getId(), newNode);
         }
 
+        //удаление узла из двусвязного списка
         private void removeNode(Node node) {
             if (node.prev != null) {
                 node.prev.next = node.next;
@@ -60,6 +67,7 @@ import java.util.HashMap;
             nodeMap.remove(node.task.getId());
         }
 
+        //Переобразовываю двусвязный список в обычный список задач
         private ArrayList<Task> getTasks() {
             ArrayList<Task> tasks = new ArrayList<>();
             Node current = head;
--- src/manager/Node.java
+++ src/manager/Node.java
@@ -3,11 +3,13 @@ package manager;
 import model.Task;
 
 public class Node {
-    Task task;
-    Node prev;
-    Node next;
+    Task task; //Ссылка на задачу
+    Node prev; //Ссылка на предыдущий узел
+    Node next; //Ссылка на следующий узел
 
+    //Конструктор для создания узла к указанной задаче
     public Node(Task task) {
+
         this.task = task;
     }
 }
