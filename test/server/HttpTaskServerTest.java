1bb3acdbdf4fc1cc1f9e57b4c257064fa8977ca4
Magomed Keligov <kms_91@list.ru>
2025-05-15 15:34:44 +0300

    ФЗ 9. Готовое v1.8

--- test/server/HttpTaskServerTest.java
+++ test/server/HttpTaskServerTest.java
@@ -3,6 +3,7 @@ import manager.TaskManager;
 import manager.Managers;
 import model.Task;
 import model.Status;
+import com.google.gson.Gson;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,18 +15,21 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HttpTaskServerTest {
     private HttpTaskServer taskServer;
     private TaskManager manager;
+    private Gson gson;
 
     @BeforeEach
     public void setUp() throws IOException {
         manager = Managers.getDefault();
         taskServer = new HttpTaskServer(manager);
         taskServer.start();
+        gson = new Gson();
     }
 
     @AfterEach
@@ -35,23 +39,30 @@ public class HttpTaskServerTest {
 
     @Test
     public void testAddTask() throws IOException, InterruptedException {
-        Task task = new Task("Test Task", "Description", Status.NEW);
-        task.setDuration(Duration.ofMinutes(30));
-        task.setStartTime(LocalDateTime.now());
+        // Создаём задачу
+        Task task = new Task("Test 2", "Testing task 2",
+                Status.NEW);
+        // Конвертируем её в JSON
+        String taskJson = gson.toJson(task);
 
+        // Создаём HTTP-клиент и запрос
         HttpClient client = HttpClient.newHttpClient();
-        String taskJson = "{\"name\":\"Test Task\",\"description\":\"Description\",\"status\":\"NEW\",\"duration\":30,\"startTime\":\"" + LocalDateTime.now() + "\"}";
-
+        URI url = URI.create("http://localhost:8080/tasks");
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/tasks"))
-                .header("Content-Type", "application/json")
+                .uri(url)
                 .POST(HttpRequest.BodyPublishers.ofString(taskJson))
                 .build();
 
+        // Вызываем рест, отвечающий за создание задач
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        // Проверяем код ответа
+        assertEquals(201, response.statusCode());
 
-        // Проверяем, что статус ответа 401 (Created)
-        assertEquals(404, response.statusCode());
+        // Проверяем, что создалась одна задача с корректным именем
+        List<Task> tasksFromManager = manager.getAllTasks();
+
+        assertNotNull(tasksFromManager, "Задачи не возвращаются");
+        assertEquals(1, tasksFromManager.size(), "Некорректное количество задач");
+        assertEquals("Test 2", tasksFromManager.get(0).getName(), "Некорректное имя задачи");
     }
 }
-
